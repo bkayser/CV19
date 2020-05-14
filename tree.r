839c8a62a7bba7d8bf89e33179044c1e34d26052
Bill Kayser <bill@kayser.org>
2020-05-14 14:03:06 -0700

    viz data generation

---
+++ tree.r
@@ -0,0 +1,41 @@
+
+library(tidyverse)
+library(scales)
+library(caTools)
+
+cvdata.us <- readRDS('data/cvdata.us.RDS')
+
+# Create a nestedn hierarchy of node structures:
+#
+# { 
+#    name: "name",
+#    children: [
+#      node,
+#      node,
+#      ...
+#    ]
+#  }
+# 
+# {
+#    name: "name", value: "value"
+# }
+#
+#
+#  Division => State => County => [ Cases, Deaths, Cases.Per100K, Deaths.Per100K ]
+#
+latest <- filter(cvdata.us, Date == max(cvdata.us$Date)) %>% arrange(Division, State, County) 
+
+concat <- function(df) {
+  paste0('{ "name": "flare", "children": [ \n',
+         split(df, fct_drop(df$Division)) %>%
+           lapply(function(division) { 
+             paste0(' { "name": "', division$Division[1],'", "children": [\n',
+                    split(division, fct_drop(division$State)) %>%
+                      lapply(function(state){
+                        paste0('{"name": "', state$State[1], '", "children": [\n',
+                               paste0('{"name": "', state$County, '", "value": ', state$Cases, '}', collapse=", "),
+                               '\n]}')
+                      }) %>% str_c(collapse=","),
+                    ']}')}) %>% str_c(collapse=","),
+         '\n]}\n')
+}
