e14b4ffff2abb3655bd0749410c3f238ff4d685f
Bill Kayser <bill@kayser.org>
2020-06-07 20:02:29 -0700

    Automate app update

--- update_reports.R
+++ update_reports.R
@@ -1,8 +1,19 @@
 rmarkdown::render('CleanData.Rmd')
 rmarkdown::render('States.Rmd')
 rmarkdown::render('International.Rmd')
+
+rsconnect::deployApp(appDir = "~/Projects/Covid19", 
+                     appFiles = c('server.R','ui.R', 'data/cvdata.us.by_state.RDS', 'data/cvdata.us.RDS', 'data/orders.events.RDS'),
+                     account = "surprise", 
+                     server = "shinyapps.io", 
+                     appName = "COVID19Demo",      
+                  #   appId = 2054587, 
+                     lint = FALSE,
+                     metadata = list(asMultiple = FALSE, asStatic = FALSE,ignoredFiles = "_app.R|.gitmodules|.RData|CleanData.nb.html|CleanData.pdf|CleanData.Rmd|LICENSE|README.md"),     
+                     logLevel = "verbose") 
+
 #rmarkdown::render('Predict.Rmd')
 #system("open Predict.nb.html")
-system("open CleanData.nb.html")
+system("open https://surprise.shinyapps.io/COVID19Demo")
 system("open States.nb.html")
 system("open International.nb.html")
